a951ae9efb64933f702c222f5ff1965b5ed4733a
Bastian Allgeier <bastian.allgeier@gmail.com>
2014-03-18 14:06:01 +0100

    new form field types

---
+++ fields/url/html.php
@@ -0,0 +1,9 @@
+<div class="form__field">
+  <label class="form__label"><?php echo html($label) ?></label>
+  <div class="form__inputWrapper">
+    <input class="form__input form__input--with-icon" type="url" placeholder="http://" ng-model="<?php echo $name ?>">
+    <span class="form__inputIcon">
+      <i class="fa fa-chain"></i>
+    </span>
+  </div>
+</div>
