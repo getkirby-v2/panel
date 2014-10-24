34de0688677d287ef63f602f2659a183375c3599
Bastian Allgeier <mail@bastianallgeier.com>
2014-10-24 14:20:43 +0200

    Fix  #256

--- app/lib/filedata.php
+++ app/lib/filedata.php
@@ -5,6 +5,7 @@ class FileData {
   static public function createByInput($file, $input = array()) {
     $data = array();
     foreach($file->meta()->toArray() as $key => $value) {
+      if(strtolower($key) == 'sort') continue;
       $data[$key] = null;
     }
     return array_merge($data, $input);
