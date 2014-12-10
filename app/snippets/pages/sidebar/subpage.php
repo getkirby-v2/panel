1a03ae0bf071ead8397014aef40156711167f280
Bastian Allgeier <mail@bastianallgeier.com>
2014-12-10 17:33:02 +0100

    Fixed broken attributes in sidebar (Fix #304)

--- app/snippets/pages/sidebar/subpage.php
+++ app/snippets/pages/sidebar/subpage.php
@@ -1,5 +1,5 @@
 <li>
-  <a class="draggable" data-helper="<?php __($subpage->title()) ?>" data-text="<?php echo dragText($subpage) ?>" href="<?php _u($subpage, 'show') ?>">
+  <a class="draggable" data-helper="<?php echo esc($subpage->title(), 'attr') ?>" data-text="<?php echo esc(dragText($subpage), 'attr') ?>" href="<?php _u($subpage, 'show') ?>">
     <?php i($subpage) ?><span><?php __($subpage->title()) ?></span>
     <small class="marginalia shiv shiv-left shiv-white"><?php __(n($subpage)) ?></small>
   </a>
