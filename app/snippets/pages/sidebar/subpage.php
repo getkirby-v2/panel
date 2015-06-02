11fae42ca5e764f4d41e3ed3a620a8476bfb9568
malvese <philippe@malvese.com>
2015-06-02 15:45:27 +0200

    Add an invisible class to sidebar subpages

--- app/snippets/pages/sidebar/subpage.php
+++ app/snippets/pages/sidebar/subpage.php
@@ -1,5 +1,5 @@
 <li>
-  <a class="draggable" data-helper="<?php echo esc($subpage->title(), 'attr') ?>" data-text="<?php echo esc(dragText($subpage), 'attr') ?>" href="<?php _u($subpage, 'show') ?>">
+  <a class="<?php e($subpage->isInvisible(), 'invisible '); ?>draggable" data-helper="<?php echo esc($subpage->title(), 'attr') ?>" data-text="<?php echo esc(dragText($subpage), 'attr') ?>" href="<?php _u($subpage, 'show') ?>">
     <?php i($subpage) ?><span><?php __($subpage->title()) ?></span>
     <small class="marginalia shiv shiv-left shiv-white"><?php __(n($subpage)) ?></small>
   </a>
