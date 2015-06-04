52163dfa926378f7bdc4bb0f2ea6d6de6de6321a
malvese <philippe@malvese.com>
2015-06-04 20:03:49 +0200

    Move class to parent list-item

--- app/snippets/pages/sidebar/subpage.php
+++ app/snippets/pages/sidebar/subpage.php
@@ -1,5 +1,5 @@
-<li>
-  <a class="<?php e($subpage->isInvisible(), 'invisible '); ?>draggable" data-helper="<?php echo esc($subpage->title(), 'attr') ?>" data-text="<?php echo esc(dragText($subpage), 'attr') ?>" href="<?php _u($subpage, 'show') ?>">
+<li class="<?php e($subpage->isInvisible(), 'invisible'); ?>">
+  <a class="draggable" data-helper="<?php echo esc($subpage->title(), 'attr') ?>" data-text="<?php echo esc(dragText($subpage), 'attr') ?>" href="<?php _u($subpage, 'show') ?>">
     <?php i($subpage) ?><span><?php __($subpage->title()) ?></span>
     <small class="marginalia shiv shiv-left shiv-white"><?php __(n($subpage)) ?></small>
   </a>
