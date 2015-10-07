e2dc9e5c7e9aa5df812a985b4733e2e47761d9bf
Bastian Allgeier <mail@bastianallgeier.com>
2015-10-07 13:12:48 +0200

    New site model tests

--- tests/tests/SiteModelTest.php
+++ tests/tests/SiteModelTest.php
@@ -26,6 +26,14 @@ class SiteModelTest extends PanelTestCase {
     $this->assertInstanceOf('Kirby\\Panel\\Models\\Page\\Changes', $this->site->changes());
   }
 
+  public function testFiles() {
+    $this->assertInstanceOf('Kirby\\Panel\\Collections\\Files', $this->site->files());    
+  }
+
+  public function testChildren() {
+    $this->assertInstanceOf('Kirby\\Panel\\Collections\\Children', $this->site->children());    
+  }
+
   public function testUpdate() {
 
     $this->site->update(array(
@@ -36,12 +44,64 @@ class SiteModelTest extends PanelTestCase {
 
   }
 
+  public function testSidebar() {
+    $this->assertInstanceOf('Kirby\\Panel\\Models\\Page\\Sidebar', $this->site->sidebar());    
+  }
+
+  public function testUpload() {
+    // not testable
+  }
+
+  public function testAddButton() {
+    $this->assertInstanceOf('Kirby\\Panel\\Models\\Page\\AddButton', $this->site->addButton());    
+  }
+
+  public function testTopbar() {
+    // TODO
+  }
+
+  public function testUsers() {
+    $this->assertInstanceOf('Kirby\\Panel\\Collections\\Users', $this->site->users());    
+  }
+
+  public function testUser() {
+    $this->assertInstanceOf('Kirby\\Panel\\Models\\User', $this->site->user());    
+  }
+
   /**
    * @expectedException Exception
    * @expectedExceptionMessage The site cannot be deleted
    */
   public function testDelete() {
     $this->site->delete();
+  }
+
+  public function testMaxSubpages() {
+    $this->assertTrue(is_int($this->site->maxSubpages()));    
+  }
+
+  public function testMaxFiles() {
+    $this->assertTrue(is_int($this->site->maxFiles()));    
+  }
+
+  public function testCanHaveFiles() {
+    $this->assertTrue(is_bool($this->site->canHaveFiles()));    
+  }
+
+  public function testCanShow() {
+    $this->assertTrue(is_bool($this->site->canShowFiles()));    
+  }
+
+  public function testCanHaveMoreSubpages() {
+    $this->assertTrue(is_bool($this->site->canHaveMoreSubpages()));    
+  }
+
+  public function testCanHaveMoreFiles() {
+    $this->assertTrue(is_bool($this->site->canHaveMoreFiles()));    
+  }
+
+  public function testStructure() {
+    $this->assertInstanceOf('Kirby\\Panel\\Models\\Page\\Structure', $this->site->structure('test'));    
   }
 
 }
