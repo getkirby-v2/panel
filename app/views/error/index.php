0950b6b5eb6b1f5a89fba1fc78c5b042efc931c1
Bastian Allgeier <mail@bastianallgeier.com>
2015-10-13 21:00:22 +0200

    Removed raw debugging on panel error pages

--- app/views/error/index.php
+++ app/views/error/index.php
@@ -1,9 +1,4 @@
 <div class="section">
   <h2 class="hgroup cf"><?php _l('error.headline') ?></h2>
   <p><?php __($text) ?></p>
-
-  <pre>
-    <code><?php var_dump($exception->getTrace()) ?></code>
-  </pre>
-
 </div>
