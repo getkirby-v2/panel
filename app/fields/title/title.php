dea2f4898a7418445ee8f657232292a289d419af
Nico Hoffmann <github@ohnehin.com>
2015-11-10 11:17:25 +0100

    Base TitleField on TextField not InputField

--- app/fields/title/title.php
+++ app/fields/title/title.php
@@ -1,18 +1,13 @@
 <?php
 
-class TitleField extends InputField {
+class TitleField extends TextField {
 
   public function __construct() {
 
-    $this->type     = 'text';
     $this->label    = l::get('fields.title.label', 'Title');
     $this->icon     = 'font';
     $this->required = true;
 
-  }
-
-  public function type() {
-    return $this->type = 'text';
   }
 
   public function help() {
