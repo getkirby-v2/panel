930aebae13ae87f5c3b1344c3dd6347f2cdfeebf
Nico Hoffmann <services@nhoffmann.com>
2016-02-06 14:55:35 +0100

    Fix #715

--- app/fields/structure/forms/update.php
+++ app/fields/structure/forms/update.php
@@ -1,8 +1,8 @@
 <?php 
 
 return function($model, $store, $entry) {
-
-  $form = new Kirby\Panel\Form($store->fields(), $entry->toArray());
+  
+  $form = new Kirby\Panel\Form($store->fields(), $entry->toArray(), $store->field());
 
   $form->cancel($model);
   $form->buttons->submit->value = l('ok');
