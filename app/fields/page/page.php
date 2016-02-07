2cd208e7cb1afe1d9a2ccedbfec50c2cfcb991ac
Nico Hoffmann <services@nhoffmann.com>
2016-02-07 11:46:47 +0100

    Page field: autocomplete options added
    Added options for `uri`, `index` and `template`:
    
    ```
    apage:
        label Page test
        type: page
       index: siblings
        template:
          - project
          - home
    ```
    
    Advances on #656

--- app/fields/page/page.php
+++ app/fields/page/page.php
@@ -7,16 +7,23 @@ class PageField extends TextField {
     $this->icon        = 'chain';
     $this->label       = l::get('fields.page.label', 'Page');
     $this->placeholder = l::get('fields.page.placeholder', 'path/to/page');
+    $this->index       = 'pages';
+    $this->template    = false;
 
   }
 
   public function input() {
 
     $input = parent::input();
-    $input->data(array(
-      'field' => 'autocomplete',
-      'url'   => panel()->urls()->api() . '/autocomplete/uris'
-    ));
+    $input->data(array('field' => 'autocomplete'));
+
+    $query = array(
+      'uri'       => $this->page()->id(),
+      'index'     => $this->index(),
+      'template'  => $this->template
+    );
+
+    $input->data('url', panel()->urls()->api() . '/autocomplete/uris?' . http_build_query($query));
 
     return $input;
 
