e7effb07d68bb3015e39008a3239166c6a6b4f68
Redian Ibra <redian@dred.com>
2016-04-13 17:32:10 +0100

    Adding Pagination on the Pages sidebar on the Panel Dashboard. This is useful when you have a huge nr of of pages with a flat url structure. It will make the Pages widget faster and more manageable.

--- app/widgets/pages/pages.html.php
+++ app/widgets/pages/pages.html.php
@@ -1,5 +1,13 @@
+
+<?php $pagination = $pages->pagination();?>
+
 <ul class="nav nav-list sidebar-list">
   <?php foreach($pages as $c): ?>
   <?php echo new Kirby\Panel\Snippet('pages/sidebar/subpage', array('subpage' => $c)) ?>
   <?php endforeach ?>
 </ul>
+
+<?php echo new Kirby\Panel\Snippet('pagination', array(
+    'pagination' => $pagination, 
+    'nextUrl' => $pagination->nextPageURL(),
+    'prevUrl' => $pagination->prevPageURL())) ?>
