bc95bd9e2e640f184439f63610e7955d36fc33f9
Bastian Allgeier <mail@bastianallgeier.com>
2016-08-03 10:57:29 +0200

    Changed version number to 2.3.2

--- app/src/panel.php
+++ app/src/panel.php
@@ -33,7 +33,7 @@ use Kirby\Panel\Models\Page\Blueprint as PageBlueprint;
 
 class Panel {
 
-  static public $version = '2.3.1';
+  static public $version = '2.3.2';
 
   // minimal requirements
   static public $requires = array(
