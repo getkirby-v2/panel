98a0d110f36608be6b477de6cb10cb5b5d6f3989
Lukas Bestle <mail@lukasbestle.com>
2016-08-29 16:13:15 +0200

    Add checkPermissions() helper to Event class

--- app/src/panel/event.php
+++ app/src/panel/event.php
@@ -2,9 +2,30 @@
 
 namespace Kirby\Panel;
 
+use Error;
 use Kirby\Event as KirbyEvent;
 
 class Event extends KirbyEvent {
+
+  /**
+   * Checks if the current user has permission for the event
+   *
+   * @param mixed $args Additional arguments for the permission callbacks
+   * @param boolean $throw If true, throws if user has no permission
+   * @return boolean
+   */
+  public function checkPermissions($args = [], $throw = false) {
+    $user = $this->user();
+    if(!$user) throw new Error('No user is logged in, cannot check permissions.');
+    $result = $user->permission($this, $args);
+
+    // set default error message if no custom one is set
+    $message = $result->message();
+    if(!$message) $message = l('permissions.error');
+
+    if($throw && !$result->status()) throw new Error($message);
+    return $result->status();
+  }
 
   /**
    * Helper methods
