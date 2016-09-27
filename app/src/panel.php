1ef29c5ec5fb8175ab58ed2fdac11fe35cc05f8b
Bastian Allgeier <mail@bastianallgeier.com>
2016-09-27 17:19:22 +0200

    Updated the version number to 2.4.0

--- app/src/panel.php
+++ app/src/panel.php
@@ -34,13 +34,13 @@ use Kirby\Panel\Models\Page\Blueprint as PageBlueprint;
 
 class Panel {
 
-  static public $version = '2.3.2';
+  static public $version = '2.4.0';
 
   // minimal requirements
   static public $requires = array(
     'php'     => '5.4.0',
-    'toolkit' => '2.2.2',
-    'kirby'   => '2.2.1'
+    'toolkit' => '2.4.0',
+    'kirby'   => '2.4.0'
   );
 
   static public $instance;
