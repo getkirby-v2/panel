f1ed1f21bbfbdc3302423ceede1e026e38344a20
Nur Bar <nbfbce@ymail.com>
2017-04-14 14:10:26 -0300

    Added pagination to pages widget

--- app/widgets/pages/pages.html.php
+++ app/widgets/pages/pages.html.php
@@ -3,3 +3,21 @@
   <?php echo new Kirby\Panel\Snippet('pages/sidebar/subpage', array('subpage' => $c)) ?>
   <?php endforeach ?>
 </ul>
+<?php if ($pages->pagination()->hasPages()) { ?>
+    <nav class="pagination" style="text-align: center">
+        <a class="pagination-prev<?= !$pages->pagination()->hasPrevPage() ? ' pagination-inactive' : '' ?>"
+           href="<?= $pages->pagination()->prevPageURL() ?>">
+            <span><i class="fa fa-arrow-left" aria-hidden="true"></i>Previous Page</span>
+        </a>
+
+        <span class="pagination-index" style="left: auto; right: auto; display: inline">
+		<?php foreach ($pages->pagination()->range(5) as $page) { ?>
+            <a href="<?= $pages->pagination()->pageURL($page) ?>" style="display: inline;<?= ($pages->pagination()->page() != $page) ? 'color: gray' : ''?>"><?= $page ?></a>
+		<?php } ?>
+		</span>
+        <a class="pagination-next<?= !$pages->pagination()->hasNextPage() ? ' pagination-inactive' : '' ?>"
+           href="<?= $pages->pagination()->nextPageURL() ?>">
+            <span>Next Page <i class="fa fa-arrow-right" aria-hidden="true"></i></span>
+        </a>
+    </nav>
+<?php } ?>
