7b8b2efa47db5a92a9319e7d9979065e8d2e5e54
Lukas Bestle <mail@lukasbestle.com>
2017-11-12 20:57:18 +0100

    Fix SVG XSS issue

--- app/views/files/edit.php
+++ app/views/files/edit.php
@@ -19,9 +19,7 @@
     </nav>
 
     <a title="<?php _l('files.show.open') ?> (o)" data-shortcut="o" target="_blank" class="fileview-image-link fileview-preview-link" href="<?php __($file->url('preview')) ?>">
-      <?php if($file->extension() == 'svg'): ?>
-      <object data="<?php __($file->url('preview')) ?>"></object>
-      <?php elseif($file->canHavePreview()): ?>
+      <?php if($file->canHavePreview()): ?>
       <img src="<?php __($file->url('preview')) ?>" alt="<?php __($file->filename()) ?>">
       <?php else: ?>
       <span>
